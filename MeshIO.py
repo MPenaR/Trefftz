ff2bc24af194e07a3f7c9f224bc9ba19651a3f6a
Manuel Pena <mpena991@gmail.com>
2025-12-15 16:49:16 +0100

    for the moment we are not requiring ngsolve as it is giving instalation problems

--- MeshIO.py
+++ MeshIO.py
@@ -5,6 +5,8 @@ import numpy as np
 from matplotlib.tri import Triangulation
 import matplotlib.pyplot as plt 
 
+# import ngsolve as ns
+
 DIM: Final = 2
 
 
@@ -12,9 +14,27 @@ class Mesh():
     '''Holds only the relevant data
     as numpy structured-arrays for easy manipulation'''
 
-    def from_matplotlib(self, Tri: Triangulation ):
-        self.points = np.column_stack([Tri.x, Tri.y])
+    def from_matplotlib(self, Tri: Triangulation):
+        '''Constructs the object from a Matptlotlib.tri.Triangulation'''
+        self._points = np.column_stack([Tri.x, Tri.y])
+        self._triangles = Tri.triangles
+        self._edges = Tri.edges 
         self.get_cell = Tri.get_trifinder()
+    
+    # def from_netgen(self, Tri: ns.Mesh):
+    #     pass
+
+    @property
+    def n_points(self) -> np.int64:
+        return self._points.shape[0]
+
+    @property
+    def n_edges(self) -> np.int64:
+        return self._edges.shape[0]
+        
+
+    def generate_Edges(self):
+        pass
 
 
 
@@ -29,10 +49,7 @@ if __name__ == "__main__":
 
     mesh = Mesh()
     mesh.from_matplotlib(Tri)
-    print(Tri.edges)
-
-    print(mesh.points)
-
+    
     plt.triplot(Tri)
     plt.show()   
 
--- test_MeshIO.py
+++ test_MeshIO.py
@@ -2,6 +2,7 @@ from MeshIO import Mesh
 from matplotlib.tri import Triangulation
 import numpy as np
 
+
 def test_from_matplotlib():
     x = np.array([0, 1, 1, 0])
     y = np.array([0, 0, 1, 1])
@@ -11,4 +12,5 @@ def test_from_matplotlib():
 
     mesh = Mesh()
     mesh.from_matplotlib(Tri)
-    assert mesh.get_cell(0.2, 0.5) == 1 and mesh.get_cell(0.5, 0.2) == 0
+    assert mesh.get_cell(0.2, 0.5) == 1 and mesh.get_cell(0.5, 0.2) == 0 and mesh.n_points == 4 and mesh.n_edges == 5
+
