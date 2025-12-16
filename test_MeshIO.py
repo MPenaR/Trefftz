39ae388b30fdd14585e7f93bcaccf6ccc95d71f0
Manuel Pena <mpena991@gmail.com>
2025-12-16 01:01:11 +0100

    matplotlib get cell working in a vectorized manner

--- test_MeshIO.py
+++ test_MeshIO.py
@@ -10,9 +10,9 @@ def test_from_matplotlib():
                           [2, 3, 0]])
     Tri = Triangulation(x=x, y=y, triangles=triangles)
 
-    # mesh = Mesh()
-    # mesh.from_matplotlib(Tri)
-    # assert mesh.get_cell(0.2, 0.5) == 1 and mesh.get_cell(0.5, 0.2) == 0 and mesh.n_points == 4 and mesh.n_edges == 5
-
     mesh = Mesh_from_Matplotlib(Tri=Tri)
-    assert mesh.get_cell(np.array([0.2, 0.5])) == 1 and mesh.get_cell(np.array([0.5, 0.2])) == 0 and mesh.n_points == 4 and mesh.n_edges == 5
+    # assert mesh.get_cell(np.array([0.2, 0.5])) == 1 and mesh.get_cell(np.array([0.5, 0.2])) == 0 and mesh.n_points == 4 and mesh.n_edges == 5
+    p = np.array([[0.2, 0.5],
+                  [0.5, 0.2]])
+    indexes = np.array([1, 0])
+    assert np.array_equal(mesh.get_cell(p), indexes) and mesh.n_points == 4 and mesh.n_edges == 5
