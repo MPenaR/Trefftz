e09257fc2319e2b67f67eaf6596e480b8ff5f785
Manuel Pena <mpena991@gmail.com>
2026-01-11 01:55:02 +0100

    switching to the new implementation as it seems to work better, and the waring were corrected

--- problems/waveguide/__init__.py
+++ problems/waveguide/__init__.py
@@ -1,5 +1,5 @@
 from .geometries import CleanWaveGuide
 from .plot import plot_waveguide
-from .base import Waveguide, Waveguide2
+from .base import Waveguide
 
-__all__ =["CleanWaveGuide", "plot_waveguide", "Waveguide", "Waveguide2"]
+__all__ =["CleanWaveGuide", "plot_waveguide", "Waveguide"]
--- problems/waveguide/base.py
+++ problems/waveguide/base.py
@@ -117,112 +117,112 @@ class Scatterer:
 
 
 
-class Waveguide:
-    def __init__(self, H: float = 1., R: float = 5., lc: float = 0.3,
-                 scatterers: Optional[tuple[Scatterer]] = None, verbose: bool = False):
-        self.R = R
-        self.H = H
-        self.verbose = verbose
-        gmsh.initialize()
-        gmsh.option.setNumber("General.Terminal", int(verbose))
-        gmsh.model.add("Waveguide")
-        p0 = gmsh.model.geo.addPoint(-R, 0., 0., lc)
-        p1 = gmsh.model.geo.addPoint( R, 0., 0., lc)
-        p2 = gmsh.model.geo.addPoint( R,  H, 0., lc)
-        p3 = gmsh.model.geo.addPoint(-R,  H, 0., lc)
+# class Waveguide:
+#     def __init__(self, H: float = 1., R: float = 5., lc: float = 0.3,
+#                  scatterers: Optional[tuple[Scatterer]] = None, verbose: bool = False):
+#         self.R = R
+#         self.H = H
+#         self.verbose = verbose
+#         gmsh.initialize()
+#         gmsh.option.setNumber("General.Terminal", int(verbose))
+#         gmsh.model.add("Waveguide")
+#         p0 = gmsh.model.geo.addPoint(-R, 0., 0., lc)
+#         p1 = gmsh.model.geo.addPoint( R, 0., 0., lc)
+#         p2 = gmsh.model.geo.addPoint( R,  H, 0., lc)
+#         p3 = gmsh.model.geo.addPoint(-R,  H, 0., lc)
         
-        bottom = gmsh.model.geo.addLine(p0, p1)
-        right  = gmsh.model.geo.addLine(p1, p2)
-        top    = gmsh.model.geo.addLine(p2, p3)
-        left   = gmsh.model.geo.addLine(p3, p0)
+#         bottom = gmsh.model.geo.addLine(p0, p1)
+#         right  = gmsh.model.geo.addLine(p1, p2)
+#         top    = gmsh.model.geo.addLine(p2, p3)
+#         left   = gmsh.model.geo.addLine(p3, p0)
 
-        boundary = gmsh.model.geo.addCurveLoop([bottom, right, top, left])
-        domain = gmsh.model.geo.addPlaneSurface([boundary])
-        gmsh.model.addPhysicalGroup(2, [domain], Region.OMEGA, "Omega")
-        gmsh.model.addPhysicalGroup(1, [bottom, top], Region.GAMMA, "Gamma")
-        # gmsh.model.addPhysicalGroup(1, [left], Region.SIGMA_L, "Sigma_L")
-        # gmsh.model.addPhysicalGroup(1, [right], Region.SIGMA_R, "Sigma_R")
-        gmsh.model.addPhysicalGroup(1, [left, right], Region.SIGMA, "Sigma")
+#         boundary = gmsh.model.geo.addCurveLoop([bottom, right, top, left])
+#         domain = gmsh.model.geo.addPlaneSurface([boundary])
+#         gmsh.model.addPhysicalGroup(2, [domain], Region.OMEGA, "Omega")
+#         gmsh.model.addPhysicalGroup(1, [bottom, top], Region.GAMMA, "Gamma")
+#         # gmsh.model.addPhysicalGroup(1, [left], Region.SIGMA_L, "Sigma_L")
+#         # gmsh.model.addPhysicalGroup(1, [right], Region.SIGMA_R, "Sigma_R")
+#         gmsh.model.addPhysicalGroup(1, [left, right], Region.SIGMA, "Sigma")
         
-        gmsh.model.geo.synchronize()
-        gmsh.model.mesh.generate(2)
-        # gmsh.fltk.run()
-        # gmsh.write('CleanWaveguide.msh')
-        # gmsh.finalize()
-        # self._domain = TrefftzMesh.from_gmsh('CleanWaveguide.msh')
-        points, edges, triangles, edges2triangles, locator, cell_sets = GmshArrays(gmsh.model)
-        gmsh.finalize()
+#         gmsh.model.geo.synchronize()
+#         gmsh.model.mesh.generate(2)
+#         # gmsh.fltk.run()
+#         # gmsh.write('CleanWaveguide.msh')
+#         # gmsh.finalize()
+#         # self._domain = TrefftzMesh.from_gmsh('CleanWaveguide.msh')
+#         points, edges, triangles, edges2triangles, locator, cell_sets = GmshArrays(gmsh.model)
+#         gmsh.finalize()
 
-        # construction cell sets (regions)
+#         # construction cell sets (regions)
 
-        self._domain = TrefftzMesh(points, edges, triangles, edges2triangles, locator, cell_sets)
-        self._regions = Region
-        return
+#         self._domain = TrefftzMesh(points, edges, triangles, edges2triangles, locator, cell_sets)
+#         self._regions = Region
+#         return
     
-    @property
-    def domain(self) -> "TrefftzMesh":
-        return self._domain
-    
-
-    def plot(self, figsize: Optional[tuple[int, int]] = (16, 2), line_width: Optional[int] = 1):
-        from matplotlib.collections import LineCollection
-        _, ax = plt.subplots(figsize=figsize)
-        # ax.triplot(Triangulation(x=M._points[:,0], y=M._points[:,1], triangles=M._triangles),linewidth=lw, color='k')
-    
-        # S = self.domain._cell_sets["S"]["line"]
-        # G = self.domain._cell_sets["Gamma"]["line"]  # it allows for multidimensional subsets
-        # inner = np.where(self.domain.edges["type"] == EdgeType.INNER)[0]
-
-        S = np.where(self.domain.edges["region"] == Region.SIGMA)[0]
-        G = np.where(self.domain.edges["region"] == Region.GAMMA)[0]  
-        inner = np.where(self.domain.edges["type"] == EdgeType.INNER)[0]
+#     @property
+#     def domain(self) -> "TrefftzMesh":
+#         return self._domain
     
 
+#     def plot(self, figsize: Optional[tuple[int, int]] = (16, 2), line_width: Optional[int] = 1):
+#         from matplotlib.collections import LineCollection
+#         _, ax = plt.subplots(figsize=figsize)
+#         # ax.triplot(Triangulation(x=M._points[:,0], y=M._points[:,1], triangles=M._triangles),linewidth=lw, color='k')
+    
+#         # S = self.domain._cell_sets["S"]["line"]
+#         # G = self.domain._cell_sets["Gamma"]["line"]  # it allows for multidimensional subsets
+#         # inner = np.where(self.domain.edges["type"] == EdgeType.INNER)[0]
 
-        ax.add_collection(LineCollection(np.stack([self.domain.edges[inner]["P"],
-                                                   self.domain.edges[inner]["Q"]], axis=1),
-                                                   colors='k', linewidths=line_width))
-
-        ax.add_collection(LineCollection(np.stack([self.domain.edges[S]["P"],
-                                                   self.domain.edges[S]["Q"]], axis=1),
-                                                   colors='r', linewidths=line_width))
-
-        ax.add_collection(LineCollection(np.stack([self.domain.edges[G]["P"],
-                                                   self.domain.edges[G]["Q"]], axis=1),
-                                                   colors='b', linewidths=line_width))
-
-
-        ax.axis('equal')
-        ax.axis('off')
-        plt.show()
+#         S = np.where(self.domain.edges["region"] == Region.SIGMA)[0]
+#         G = np.where(self.domain.edges["region"] == Region.GAMMA)[0]  
+#         inner = np.where(self.domain.edges["type"] == EdgeType.INNER)[0]
     
 
-    def plot_field(self, u: Callable[[NDArray[Any], NDArray[Any]], NDArray[Any]],
-                   N: int = 100, figsize: Optional[tuple[int, int]] = (16, 2), real_part: bool = False):
-        x = np.linspace(-self.R, self.R, N)
-        y = np.linspace(0., self.H, N)
-        X, Y = np.meshgrid(x, y)
-        Z = u(X, Y)
 
-        if real_part:
-            Z = np.real(Z)
+#         ax.add_collection(LineCollection(np.stack([self.domain.edges[inner]["P"],
+#                                                    self.domain.edges[inner]["Q"]], axis=1),
+#                                                    colors='k', linewidths=line_width))
 
-        _, ax = plt.subplots(figsize=figsize)
+#         ax.add_collection(LineCollection(np.stack([self.domain.edges[S]["P"],
+#                                                    self.domain.edges[S]["Q"]], axis=1),
+#                                                    colors='r', linewidths=line_width))
 
-        ax.pcolorfast((-self.R, self.R), (0., self.H), Z)
-        plt.show()
+#         ax.add_collection(LineCollection(np.stack([self.domain.edges[G]["P"],
+#                                                    self.domain.edges[G]["Q"]], axis=1),
+#                                                    colors='b', linewidths=line_width))
 
 
-    def set_boundary_conditions(self, bc_dict: dict[Region, FluxType]):
-        for region, flux in bc_dict.items():
-            self.domain.edges["flux_type"][self.domain.edges["region"] == region] = flux
-        if np.all(self.domain.edges["flux_type"] >= 0):
-            if self.verbose: 
-                print('Problem ready for assembly')
-            self.domain.ready_for_assemble = True
+#         ax.axis('equal')
+#         ax.axis('off')
+#         plt.show()
+    
+
+#     def plot_field(self, u: Callable[[NDArray[Any], NDArray[Any]], NDArray[Any]],
+#                    N: int = 100, figsize: Optional[tuple[int, int]] = (16, 2), real_part: bool = False):
+#         x = np.linspace(-self.R, self.R, N)
+#         y = np.linspace(0., self.H, N)
+#         X, Y = np.meshgrid(x, y)
+#         Z = u(X, Y)
+
+#         if real_part:
+#             Z = np.real(Z)
+
+#         _, ax = plt.subplots(figsize=figsize)
+
+#         ax.pcolorfast((-self.R, self.R), (0., self.H), Z)
+#         plt.show()
 
 
-class Waveguide2(Problem):
+#     def set_boundary_conditions(self, bc_dict: dict[Region, FluxType]):
+#         for region, flux in bc_dict.items():
+#             self.domain.edges["flux_type"][self.domain.edges["region"] == region] = flux
+#         if np.all(self.domain.edges["flux_type"] >= 0):
+#             if self.verbose: 
+#                 print('Problem ready for assembly')
+#             self.domain.ready_for_assemble = True
+
+
+class Waveguide(Problem):
     def __init__(self, mesh: TrefftzMesh,
                  boundary_conditions_map: dict[Region, FluxType],
                  verbose: bool = False,
@@ -250,6 +250,8 @@ class Waveguide2(Problem):
 
         boundary = gmsh.model.geo.addCurveLoop([bottom, right, top, left])
         domain = gmsh.model.geo.addPlaneSurface([boundary])
+        gmsh.model.geo.synchronize()
+
         gmsh.model.addPhysicalGroup(2, [domain], Region.OMEGA, "Omega")
         gmsh.model.addPhysicalGroup(1, [bottom, top], Region.GAMMA, "Gamma")
         gmsh.model.addPhysicalGroup(1, [left, right], Region.SIGMA, "Sigma")
