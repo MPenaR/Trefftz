1a941bae14507c37a38daeaa89bd3c672973390a
Manuel Pena <mpena991@gmail.com>
2026-01-20 23:44:26 +0100

    NOW its working, I was comparing a mode with a normalized mode

--- trefftz/dg/serial_fluxes2.py
+++ trefftz/dg/serial_fluxes2.py
@@ -359,9 +359,9 @@ def mode_RHS2(d_m, edge, k, H, d_2, t):
     beta = np.emath.sqrt(k**2 - (t*pi/H)**2)
 
     if t == 0:
-        I = 1/sqrt(H)*2*1j*k*l*(dot(d, N) - d_2*(1-dot(d, N)))*exp(1j*k*(M_x - dot(d, M)))*sinc(k*l/(2*pi)*dot(d, T))
+        I = 2*1j*k*l*(dot(d, N) - d_2*(1-dot(d, N)))*exp(1j*k*(M_x - dot(d, M)))*sinc(k*l/(2*pi)*dot(d, T))
     else:
-        I = sqrt(2/H)*1j*k*l*(dot(d, N) - d_2*(1-k/conj(beta)*dot(d, N)))*exp(1j*(beta*M_x - k*dot(d, M)))*( exp( 1j*t*pi*dot(M,T)/H)*sinc(k*l/(2*pi)*dot(d, T) - t*(l/(2*H)))+
+        I = 1j*k*l*(dot(d, N) - d_2*(1-k/conj(beta)*dot(d, N)))*exp(1j*(beta*M_x - k*dot(d, M)))*( exp( 1j*t*pi*dot(M,T)/H)*sinc(k*l/(2*pi)*dot(d, T) - t*(l/(2*H)))+
                                                                                                              exp(-1j*t*pi*dot(M,T)/H)*sinc(k*l/(2*pi)*dot(d, T) + t*(l/(2*H))))
     return I
     #     LOCAL = 1/sqrt(H)*2*1j*k*l*(dot(d, N) - d_2*(1-dot(d, N)))*exp(1j*k*(M_x - dot(d, M)))*sinc(k*l/(2*pi)*dot(d, T))
