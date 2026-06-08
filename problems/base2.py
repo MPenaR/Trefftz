823c2052a5afb86bd9a06913e47e8251304e0644
Manuel Pena <mpena991@gmail.com>
2026-06-08 03:34:12 +0200

    assembler assembles LHS, need to think how to set RHSs

--- problems/base2.py
+++ problems/base2.py
@@ -1,71 +1,45 @@
 from trefftz.mesh import TrefftzMesh, BoundaryRegions
 from trefftz.dg.basis import PlanewaveBasis
-from typing import Protocol, TypeVar, Any, Generic
+from typing import Protocol, Generic, Optional
 from collections.abc import Mapping
 from scipy.sparse.linalg import spsolve
-from scipy.sparse import sparray, coo_array, csr_array
-from enum import Enum, EnumMeta
-from numpy.typing import NDArray
-from trefftz.dg.block_fluxes import SoundHard_block
+from scipy.sparse import coo_array# , csr_array, csc_array
+from dataclasses import dataclass
+from trefftz.numpy_types import complex_array
 import numpy as np
-from trefftz.numpy_types import int_array, complex_array
-from numpy.typing import NDArray
-
 
 
 class BoundaryCondition(Protocol):
-    def assemble_LHS(self, k: float, edge) -> tuple[complex_array, int_array, int_array]:
-        ...
+    data: Optional[object]
 
-    def assemble_RHS(self, k: float, edge) -> tuple[complex_array, int_array]:
-        ...
 
 class SoundHardBC:
-    
-    def assemble_LHS(self, k: float, edge) -> tuple[complex_array, int_array, int_array]:
-        pass
-        # for edge in edges:
-        #     A[] = SoundHard_block(k = k, edge = edge, d = d, d_d = dd, d_1 = self.d_1)
-
-    def assemble_RHS(self, k: float, edge) -> tuple[complex_array, int_array]:
-        pass
-        # for edge in edges:
-        #     b[] = SoundHard_block(k = k, edge = edge, d = d, d_d = dd, d_1 = self.d_1)
+    def __init__(self, data: object | None = None):
+        self.data = data
 
 class RadiatingBC:
-    def assemble_LHS(self, k: float, edges: NDArray[Any]) -> tuple[np.complex128, int, int]:
-        ...
+    def __init__(self, data: object | None = None):
+        self.data = data
 
-    def assemble_RHS(self, k: float, edges: NDArray[Any]) -> tuple[np.complex128, int]:
-        ...
-
-class DtNBC:
-    def __init__(self, truncating_radius: float):
+class NtDBC:
+    def __init__(self, truncating_radius: float, data: object | None = None):
         self.truncating_radius = truncating_radius
-
-    def assemble_LHS(self, k: float, edges: NDArray[Any]) -> tuple[np.complex128, int, int]:
-        ...
-
-    def assemble_RHS(self, k: float, edges: NDArray[Any]) -> tuple[np.complex128, int]:
-        ...
+        self.data = data
     
 
 
-class CircularDtN:
+class CircularNtD:
     ...
 
-class WaveguideDtN:
+class WaveguideNtD:
     ...
 class ExactSolution:
-    pass
-
-class BCType(Enum):
     pass
 
 
 class Assembler(Protocol):
 
-    def assemble_LHS(self, p: "Problem[BoundaryRegions]") -> sparray:
+    def assemble_LHS(self, p: "Problem[BoundaryRegions]") -> coo_array:
         ...
 
     # def assemble_RHS(self, p: "Problem[BoundaryRegions]") -> NDArray[Any]
@@ -86,115 +60,123 @@ class NonLocalKernel(Protocol):
 
 class SerialAssembler(Assembler):
 
-    def assemble_LHS(self, p: "Problem[BoundaryRegions]") -> sparray:
+    def assemble_LHS(self, p: "Problem[BoundaryRegions]") -> coo_array:
         rows: list[int] = []
         cols: list[int] = []
-        data: list[complex] = []
+        values: list[complex] = []
         
         # interior edges
         for edge in p.mesh.interior_edges:
-            kernel = p.numerics.interior_kernel
+            interior_kernel = p.numerics.interior_kernel
             T1, T2 = edge["triangles"]
 
             # local T1
-            for i in range(p.basis.dofs_on_element(T1)):
-                for j in range(p.basis.dofs_on_element(T1)):
-                    d_phi = p.basis.D[j]
-                    d_psi = p.basis.D[i]
-                    val = kernel.LHS(edge, d_phi, d_psi, p.k)
+            for i in p.basis.dofs_on_element(T1):
+                for j in p.basis.dofs_on_element(T1):
+                    d_phi = p.basis.global_direction(j)
+                    d_psi = p.basis.global_direction(i)
+                    val = interior_kernel.LHS(edge, d_phi, d_psi, p.k)
                     rows.append(i)
                     cols.append(j)
-                    data.append(val)
+                    values.append(val)
 
-            for i in range(p.basis.dofs_on_element(T1)):
-                for j in range(p.basis.dofs_on_element(T2)):
-                    d_phi = p.basis.D[j]
-                    d_psi = p.basis.D[i]
-                    val = kernel.LHS(edge, d_phi, d_psi, p.k)
+            for i in p.basis.dofs_on_element(T1):
+                for j in p.basis.dofs_on_element(T2):
+                    d_phi = p.basis.global_direction(j)
+                    d_psi = p.basis.global_direction(i)
+                    val = interior_kernel.LHS(edge, d_phi, d_psi, p.k)
                     rows.append(i)
                     cols.append(j)
-                    data.append(val)
+                    values.append(val)
 
-            for i in range(p.basis.dofs_on_element(T2)):
-                for j in range(p.basis.dofs_on_element(T1)):
-                    d_phi = p.basis.D[j]
-                    d_psi = p.basis.D[i]
-                    val = kernel.LHS(edge, d_phi, d_psi, p.k)
+            for i in p.basis.dofs_on_element(T2):
+                for j in p.basis.dofs_on_element(T1):
+                    d_phi = p.basis.global_direction(j)
+                    d_psi = p.basis.global_direction(i)
+                    val = interior_kernel.LHS(edge, d_phi, d_psi, p.k)
                     rows.append(i)
                     cols.append(j)
-                    data.append(val)
+                    values.append(val)
             # local T2
-            for i in range(p.basis.dofs_on_element(T2)):
-                for j in range(p.basis.dofs_on_element(T2)):
-                    d_phi = p.basis.D[j]
-                    d_psi = p.basis.D[i]
-                    val = kernel.LHS(edge, d_phi, d_psi, p.k)
+            for i in p.basis.dofs_on_element(T2):
+                for j in p.basis.dofs_on_element(T2):
+                    d_phi = p.basis.global_direction(j)
+                    d_psi = p.basis.global_direction(i)
+                    val = interior_kernel.LHS(edge, d_phi, d_psi, p.k)
                     rows.append(i)
                     cols.append(j)
-                    data.append(val)
+                    values.append(val)
 
 
 
         # boundary conditions implemented as local operators
         for region in p.regions_local_kernel:
             bc = p.boundary_conditions[region]
-            kernel = p.numerics.local_boundary_kernels[type(bc)]
+            local_kernel = p.numerics.local_boundary_kernels[type(bc)]
             for edge in p.mesh.edges_on_region(region):
                 T, _ = edge["triangles"]
                 for i in p.basis.dofs_on_element(T):
                     for j in p.basis.dofs_on_element(T):
                         d_psi = p.basis.global_direction(i)
                         d_phi = p.basis.global_direction(j)
-                        value = kernel.LHS(edge, d_phi, d_psi, p.k)
+                        value = local_kernel.LHS(edge, d_phi, d_psi, p.k)
                         rows.append(i)
                         cols.append(j)
-                        data.append(value)
+                        values.append(value)
 
         # boundary conditions implemented as non-local operators
         for region in p.regions_nonlocal_kernel:
             bc = p.boundary_conditions[region]
-            kernel = p.numerics.nonlocal_boundary_kernels[type(bc)]
+            non_local_kernel = p.numerics.nonlocal_boundary_kernels[type(bc)]
             for edge_1 in p.mesh.edges_on_region(region):
                 T_1, _ = edge_1["triangles"]
                 for edge_2 in p.mesh.edges_on_region(region):
                     T_2, _ = edge_2["triangles"]
-                    for i in range(p.basis.dofs_on_element(T_1)):
-                        for j in range(p.basis.dofs_on_element(T_2)):
-                            d_phi = p.basis.D[j]
-                            d_psi = p.basis.D[i]
-                            value = kernel.LHS(edge_1, edge_2, d_phi, d_psi, p.k)
+                    for i in p.basis.dofs_on_element(T_1):
+                        for j in p.basis.dofs_on_element(T_2):
+                            d_phi = p.basis.global_direction(j)
+                            d_psi = p.basis.global_direction(i)
+                            value = non_local_kernel.LHS(edge_1, edge_2, d_phi, d_psi, p.k)
                             rows.append(i)
                             cols.append(j)
-                            data.append(value)
-        
-        A = coo_array((data, (rows, cols)), shape=(p.N_DOF, p.N_DOF))
-        return A.tocsr() #or tocsc depends on iterative or direct
+                            values.append(value)
+
+        return coo_array((values, (rows, cols)), shape=(p.N_DOF, p.N_DOF))
     
-    # def assemble_RHS(self, p: "Problem[BoundaryRegions]") -> NDArray[Any]:
-    #     rows: list[int] = []
-    #     data: list[complex] = []
-    #     for edge in p.mesh.boundary_edges:
-    #         bc = p.boundary_conditions[edge["region"]]
-    #         kernel = SerialKernels[bc]
-    #         for i in range(p.basis.N_theta):
-    #             d_psi = p.basis.D[i]
-    #             row, value = kernel.RHS_data(edge, d_psi, p.k)
-    #             rows.append(row)
-    #             data.append(value)
-    #     b = np.zeros(p.N_DOF, dtype=np.complex128)
-    #     np.add.at(b, rows, data)
-    #     return b
+    def assemble_RHS(self, p: "Problem[BoundaryRegions]") -> complex_array:
+        rows: list[int] = []
+        values: list[complex] = []
+        
+        # boundary conditions implemented as local operators
+        for region in p.regions_local_kernel:
+            bc = p.boundary_conditions[region]
+            if bc.data is None:
+                continue
+            local_kernel = p.numerics.local_boundary_kernels[type(bc)]
+            for edge in p.mesh.edges_on_region(region):
+                T, _ = edge["triangles"]
+                for i in p.basis.dofs_on_element(T):
+                    d_psi = p.basis.global_direction(i)
+                    value = local_kernel.RHS(edge, d_psi, p.k)
+                    rows.append(i)
+                    values.append(value)
 
-
-
-from dataclasses import dataclass
-
-# @dataclass
-# class Numerics:
-#     interior_flux: TransmissionKernel
-#     local_boundary_fluxes: Mapping[type[BoundaryCondition], LocalKernel]
-#     nonlocal_boundary_fluxes: Mapping[type[BoundaryCondition], NonLocalKernel]
-
+        # boundary conditions implemented as non-local operators
+        for region in p.regions_nonlocal_kernel:
+            bc = p.boundary_conditions[region]
+            if bc.data is None:
+                continue
+            non_local_kernel = p.numerics.nonlocal_boundary_kernels[type(bc)]
+            for edge_1 in p.mesh.edges_on_region(region):
+                T_1, _ = edge_1["triangles"]
+                for i in p.basis.dofs_on_element(T_1):
+                    d_psi = p.basis.global_direction(i)
+                    value = non_local_kernel.RHS(edge_1, d_psi, p.k)
+                    rows.append(i)
+                    values.append(value)
+        b = np.zeros((p.N_DOF,), dtype=np.complex128)
+        np.add.at(b, rows, values)
+        return b
 @dataclass
 class Numerics:
     interior_kernel: TransmissionKernel
@@ -218,8 +200,8 @@ class Problem(Generic[BoundaryRegions]):
         self.u = u
         self.numerics = numerics
         self.assembler = assembler
-        self.A: sparray | None = None
-        self.b: sparray | None = None
+        self._A: coo_array | None = None
+        self._b: complex_array| None = None
 
         self._regions_local_kernel = [ region for region, bc in self.boundary_conditions.items() if type(bc) in numerics.local_boundary_kernels ]
         self._regions_nonlocal_kernel = [ region for region, bc in self.boundary_conditions.items() if type(bc) in numerics.nonlocal_boundary_kernels ]
@@ -241,48 +223,22 @@ class Problem(Generic[BoundaryRegions]):
     def assembled(self) -> bool:
         return self.A is not None and self.b is not None
 
-    # def assemble_LHS(self):
+    def assemble_LHS(self):
+        self._A = self.assembler.assemble_LHS(self)
+        return self._A
 
-    #     # Assembly of the local operators
-    #     data = []
-    #     rows = []
-    #     cols = []
-    #     ## Interior fluxes
-    #     for edge in self.mesh.interior_edges:
-    #         data_edge, rows_edge, cols_edge = TransmissionFlux.assemble(edge)
-    #         data.extend(data_edge)
-    #         rows.extend(rows_edge)
-    #         cols.extend(cols_edge)
-        
-    #     ## Boundary conditions
-    #     for region in self.mesh.boundary_regions:
-    #         for edge in self.mesh.edges_on_region(region):
-    #                         data_edge, rows_edge, cols_edge = self.boundary_conditions[region].assemble_LHS(self.k, edge)
-    #         data.extend(data_edge)
-    #         rows.extend(rows_edge)
-    #         cols.extend(cols_edge)
+    @property
+    def A(self) -> coo_array | None:
+        return self._A
 
-    #     # Assembly of the local ones
-    #     A_local = coo_array((data, (rows, cols)), shape=(self.N_DOF, self.N_DOF))
+    @property
+    def b(self) -> complex_array | None:
+        return self._b
 
-    #     #     A += self.boundary_conditions[region].assemble_LHS(k = self.k, edges = edges[region], self.N_DOF)
-        
-    #     A_non_local = coo_array((self.N_DOF, self.N_DOF), dtype=np.complex128)
-    #     self.A: csr_array = (A_local + A_non_local).tocsr()
-    
-    # def assemble_RHS(self):
-    #     b = coo_array((self.N_DOF), np.complex128)
-    #     for region in self.mesh.boundary_regions:
-    #         print(region)
-    #     #     b += self.boundary_conditions[region].assemble_RHS(k = self.k, edges = edges[region], self.N_DOF)
-    #     self.b = b
 
     def assemble_RHS(self):
         pass
         # self.A = self.assembler.assemble_RHS(self)
-
-    def assemble_LHS(self):
-        self.b = self.assembler.assemble_LHS(self)
 
     def assemble(self):
         self.assemble_RHS()
--- trefftz/dg/kernels.py
+++ trefftz/dg/kernels.py
@@ -1,4 +1,6 @@
-from numpy import dot, exp, sinc, pi
+from numpy import dot, exp, sinc, pi, conj
+from numpy.lib.scimath import sqrt
+
 
 class SoundHardKernel:
     '''Serial SoundHard kernel'''
@@ -10,10 +12,10 @@ class SoundHardKernel:
         d_m = d_psi
         d_n = d_phi
 
-        M = edge.M
-        l = edge.l
-        N = edge.N
-        T = edge.T
+        M = edge["M"]
+        l = edge["l"]
+        N = edge["N"]
+        T = edge["T"]
 
         return -1j*k*l*(1 + d_1 * dot(d_n, N))*dot(d_m, N)*exp(1j*k*dot(d_n - d_m, M)) * sinc(k*l/(2*pi)*dot(d_n-d_m, T))
 
@@ -33,10 +35,142 @@ class UltraWeakKernel:
         k_n = k 
         k_m = k
 
-        M = edge.M
-        N = edge.N
-        T = edge.T
-        l = edge.l
+        M = edge["M"]
+        l = edge["l"]
+        N = edge["N"]
+        T = edge["T"]
 
         # I = -1j*l/2*(2*a*k + k_n*dot(d_n, N) + k_m*dot(d_m, N) + 2*b/k*k_n*dot(d_n, N)*k_m*dot(d_m, N))*exp(1j*dot(k_n*d_n - k_m*d_m, M))*sinc(l/(2*pi)*dot(k_n*d_n - k_m*d_m,T))
         return -1j*k*l*( 1/2*( k_n/k*dot(d_n, N) + k_m/k*dot(d_m, N)) + a + b*k_n/k*k_m/k*dot(d_n, N)*dot(d_m, N))*exp(1j*dot(k_n*d_n - k_m*d_m, M))*sinc(l/(2*pi)*dot(k_n*d_n - k_m*d_m,T))
+
+class NtDLocal:
+    def __init__(self, R: float, d_2: float):
+        self.R = R
+        self.d_2 = d_2
+    
+    def LHS(self, edge, d_phi, d_psi, k: float) -> complex:
+        r"""
+        Computes the flux on a radiating boundary with respect to the degrees
+        of freedom from the same cell, that is:
+
+        TODO: it is assuming that the radiating boundary consists of a vertical segment. This should be easy to generalize.
+        
+        .. math::
+        
+            -\int_{E}\left(d_{2}ik\Phi_n(\mathbf{x})+\nabla \Phi_n(\mathbf{x})\cdot\mathbf{n}\right)\overline{\Psi_n(\mathbf{x})}\,\mathrm{d}S_{\mathbf{x}}
+
+        which can be computed as:
+
+        .. math::
+        
+            \boxed{-ikl\left(d_{2}+\mathbf{d}_{n}\cdot\mathbf{n}\right)e^{ik\left(\mathbf{d}_{n}-\mathbf{d}_{m}\right)\cdot\mathbf{M}}\mathrm{sinc}\left(\frac{kl}{2\pi}\left(\mathbf{d}_{n}-\mathbf{d}_{m}\right)\mathbf{j}\right)}
+
+        Parameters
+        ----------
+        phi : Function
+            Trial function.
+        psi : Function
+            Test function.
+        k : float
+            Wave number.
+        edge : Edge
+            Edge parameters.
+        d_2 : float
+            Stabilyzing parameter.
+
+        Returns
+        -------
+        I : complex
+            The integral.
+        """
+
+        d_2 = self.d_2
+        d_n = d_phi
+        d_m = d_psi
+
+
+        M = edge["M"]
+        l = edge["l"]
+        N = edge["N"]
+        T = edge["T"]
+
+        return -1j*k*l*(d_2 + dot(d_n, N))*exp(1j*k*dot(d_n - d_m, M))*sinc(k*l/(2*pi)*dot(d_n-d_m, T))
+
+
+class WaveguideNtD_nonlocal:
+    def __init__(self, R: float, d_2: float, M: int, H: float): 
+        self.R = R
+        self.d_2 = d_2 
+        self.M = M 
+        self.H = H
+
+    def LHS(self, edge_1, edge_2, d_phi, d_psi, k : float) -> complex:
+        r"""
+        Computes the flux on a radiating boundary with respect to the degrees
+        of freedom from another cell, that is:
+
+        TODO: it is assuming that the radiating boundary consists of a vertical segment. This should be easy to generalize.
+        
+        Parameters
+        ----------
+        phi : Function
+            Trial function.
+        psi : Function
+            Test function.
+        k : float
+            Wave number.
+        edge_u : Edge
+            Edge of the triangle associated to the trial function.
+        edge_v : Edge
+            Edge of the triangle associated to the test function.
+        d_2 : float
+            Stabilyzing parameter.
+        NtD_modes : int
+            Number of modes for the approximation of the NtD map.
+        H : float
+            height of the waveguide. 
+
+        Returns
+        -------
+        I : complex
+            The integral.
+
+        
+        """
+
+        d_2 = self.d_2
+        H = self.H
+        M = self.M
+
+        M_u = edge_1["M"]
+        l_u = edge_1["l"]
+
+        M_v = edge_2["M"]
+        l_v = edge_2["l"]
+
+
+        N = edge_1["N"]
+        T = edge_1["T"]
+
+        d_n = d_phi
+        d_m = d_psi
+
+        I1 = -1j*k*H*dot(d_n,N)*dot(d_m,N)*d_2*exp(1j*k*(dot(d_n,M_u) - dot(d_m,M_v)))*l_u/H*l_v/H*(
+            sinc(k*l_u/(2*pi)*d_n[1])*sinc(k*l_v/(2*pi)*d_m[1]) + 1/2*sum( [ k**2 / abs(sqrt(complex(k**2 - (s*pi/H)**2)))**2 * (
+            exp( 1j*s*pi/H*M_u[1])*sinc(k*l_u/(2*pi)*d_n[1] + s*l_u/(2*H)) + exp(-1j*s*pi/H*M_u[1])*sinc(k*l_u/(2*pi)*d_n[1] - s*l_u/(2*H)) ) *(
+            exp(-1j*s*pi/H*M_v[1])*sinc(k*l_v/(2*pi)*d_m[1] + s*l_v/(2*H)) + exp( 1j*s*pi/H*M_v[1])*sinc(k*l_v/(2*pi)*d_m[1] - s*l_v/(2*H)) )
+            for s in range(1, M)]) )
+        
+        I2 = -1j*k*H*dot(d_n,N)*(dot(d_m,N)-d_2)*exp(1j*k*(dot(d_n,M_u) - dot(d_m,M_v)))*l_u/H*l_v/H*(
+            sinc(k*l_u/(2*pi)*d_n[1])*sinc(k*l_v/(2*pi)*d_m[1]) + 1/2*sum( [ k / sqrt(complex(k**2 - (s*pi/H)**2)) * (
+            exp( 1j*s*pi*M_u[1]/H)*sinc(k*l_u/(2*pi)*d_n[1] + s*l_u/(2*H)) + exp(-1j*s*pi*M_u[1]/H)*sinc(k*l_u/(2*pi)*d_n[1] - s*l_u/(2*H)) ) *(
+            exp(-1j*s*pi*M_v[1]/H)*sinc(k*l_v/(2*pi)*d_m[1] + s*l_v/(2*H)) + exp( 1j*s*pi*M_v[1]/H)*sinc(k*l_v/(2*pi)*d_m[1] - s*l_v/(2*H)) )
+            for s in range(1, M)]) )
+        
+        I3 = 1j*k*H*dot(d_m,N)*d_2*exp(1j*k*(dot(d_n,M_u) - dot(d_m,M_v)))*l_u/H*l_v/H*(
+            sinc(k*l_u/(2*pi)*d_n[1])*sinc(k*l_v/(2*pi)*d_m[1]) + 1/2*sum( [ k / conj(sqrt(complex(k**2 - (s*pi/H)**2))) * (
+            exp( 1j*s*pi/H*M_u[1])*sinc(k*l_u/(2*pi)*d_n[1] + s*l_u/(2*H)) + exp(-1j*s*pi/H*M_u[1])*sinc(k*l_u/(2*pi)*d_n[1] - s*l_u/(2*H)) ) *(
+            exp(-1j*s*pi/H*M_v[1])*sinc(k*l_v/(2*pi)*d_m[1] + s*l_v/(2*H)) + exp( 1j*s*pi/H*M_v[1])*sinc(k*l_v/(2*pi)*d_m[1] - s*l_v/(2*H)) )
+            for s in range(1, M)]) )
+
+        return  I1 + I2 + I3
